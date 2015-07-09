ea0c2a43b5786cadbabc1e0430e684d32324ac0a
Ian MacDonald & Cory Spitzer <ian@iancmacdonald.com>
2015-07-09 14:05:52 -0700

    Create reviews

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :listings
   resources :reservations
+  resources :reviews
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/features/review_a_listing_spec.rb
+++ spec/features/review_a_listing_spec.rb
@@ -1,7 +1,18 @@
-# require 'rails_helper'
-#
-# describe 'adding a review to a listing' do
-#   it 'will add a review to a listing' do
-#
-#   end
-# end
+require 'rails_helper'
+
+describe 'adding a review to a listing' do
+  it 'will add a review to a listing' do
+    user = FactoryGirl.create :user
+    login_as user, scope: :user
+    listing = FactoryGirl.create :listing
+    user.listings.push listing
+    logout
+    user2 = FactoryGirl.create :user, name: 'b', username: 'b1', email: 'b@b.com', password: '123456789', admin: false
+    login_as user2, scope: :user
+    visit listing_path listing
+    click_on 'Review'
+    fill_in 'Content', with: 'Our stay here was great!'
+    select 5, from: "Rating"
+    click_on 'Create Review'
+  end
+end
