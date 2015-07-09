fe97c8861aeb4347714817ba2b8a147cdb1ce740
Ian MacDonald & Cory Spitzer <student@epicodus.com>
2015-07-09 11:40:38 -0700

    Fail adding a reservation when a date is not entered

--- spec/features/rent_a_listing_path_spec.rb
+++ spec/features/rent_a_listing_path_spec.rb
@@ -15,4 +15,18 @@ describe 'the rent a listing path' do
     click_on 'Rent'
     expect(page).to have_content 'Rental dates'
   end
+
+  it 'will fail letting a user rent a listing' do
+    user = FactoryGirl.create :user
+    login_as user, scope: :user
+    listing = FactoryGirl.create :listing
+    user.listings.push listing
+    logout
+    user2 = FactoryGirl.create :user, name: 'b', username: 'b1', email: 'b@b.com', password: '123456789', admin: false
+    login_as user2, scope: :user
+    visit listing_path listing
+    fill_in 'End date', with: Date.new(2015, 02, 03)
+    click_on 'Rent'
+    expect(page).to have_content 'try again'
+  end
 end
